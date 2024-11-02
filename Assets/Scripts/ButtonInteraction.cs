ab7dffe653a1b3d2153bf823cc2648331b772e01
AdrienGoups <agoupil2005@gmail.com>
2024-11-02 17:29:00 -0400

    Scripts pour la fonctionalités des boutons fini

--- Assets/Scripts/ButtonInteraction.cs
+++ Assets/Scripts/ButtonInteraction.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ButtonInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private int player1Score = 0;
+    private int player2Score = 0;
+    [SerializeField]
+    private GameObject _buttonPlayerWinObject;
+    [SerializeField]
+    private TMP_Text _buttonPlayerWinText;
+
+    private void OnPressP1()
     {
-        
+        player1Score++;
+        Debug.Log("Player 1 : " + player1Score);
+    }
+    private void OnPressP2()
+    {
+        player2Score++;
+        Debug.Log("Player 2 : " + player2Score);
+    }
+    private void OnStartTimer(){
+        player1Score = 0;
+        player2Score = 0;
+        _buttonPlayerWinObject.SetActive(false);
+        gameObject.GetComponent<ButtonTimer>().StartTimer();
+    }
+    public void CompareScore(){
+        if(player1Score > player2Score){
+            Debug.Log("Player 1 wins!");
+            _buttonPlayerWinObject.SetActive(true);
+            _buttonPlayerWinText.text = "Player 1 Wins!";
+        }
+        else if(player1Score < player2Score){
+            Debug.Log("Player 2 wins!");
+            _buttonPlayerWinObject.SetActive(true);
+            _buttonPlayerWinText.text = "Player 2 Wins!";
+        }
+        else{
+            Debug.Log("Draw!");
+            _buttonPlayerWinObject.SetActive(true);
+            _buttonPlayerWinText.text = "Draw!";
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
