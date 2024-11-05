59c1fc5bd1d9eaeb4a9613bb3942a67b3840e0f1
AdrienGoups <agoupil2005@gmail.com>
2024-11-05 14:31:36 -0500

    script de camera fini

--- Assets/Scripts/CameraDisplay.cs
+++ Assets/Scripts/CameraDisplay.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class CameraDisplay : MonoBehaviour
 {
+
     [SerializeField]
-    private GameObject camera1; 
-    [SerializeField]
-    private GameObject camera2; 
-    public void DisplayCamera1(){
-        camera2.SetActive(false);
-        camera1.SetActive(true);
-        
-    }
-    public void DisplayCamera2(){
-        camera1.SetActive(false);
-        camera2.SetActive(true);
-        
+    private Camera camera2; 
+    void Start(){
+        camera2.targetDisplay = 1; 
+        Display.displays[1].Activate();
     }
 }
