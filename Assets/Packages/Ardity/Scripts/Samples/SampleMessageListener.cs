df80f62a523c9f8d7ca087e8af759736033da705
GuiLanglois <112094107+GuiLanglois@users.noreply.github.com>
2024-11-05 04:29:22 -0500

    Test boutons Arduino
    - Ajout d'une scène de test
    - Communication entre Arduino et Unity
    - Début de tests d'interaction entre les boutons et Unity

---
+++ Assets/Packages/Ardity/Scripts/Samples/SampleMessageListener.cs
@@ -0,0 +1,42 @@
+/**
+ * Ardity (Serial Communication for Arduino + Unity)
+ * Author: Daniel Wilches <dwilches@gmail.com>
+ *
+ * This work is released under the Creative Commons Attributions license.
+ * https://creativecommons.org/licenses/by/2.0/
+ */
+
+using UnityEngine;
+using System.Collections;
+using UnityEditor.UI;
+using TMPro;
+
+/**
+ * When creating your message listeners you need to implement these two methods:
+ *  - OnMessageArrived
+ *  - OnConnectionEvent
+ */
+public class SampleMessageListener : MonoBehaviour
+{
+    // Objet texte qui affiche le message
+    [SerializeField]
+    private TMP_Text message;
+    
+    // Invoked when a line of data is received from the serial device.
+    void OnMessageArrived(string msg)
+    {
+        Debug.Log("Message arrived: " + msg);
+        message.text = msg.ToString();
+    }
+
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
+    {
+        if (success)
+            Debug.Log("Connection established");
+        else
+            Debug.Log("Connection attempt failed or disconnection detected");
+    }
+}
