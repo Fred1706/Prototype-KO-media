e1e51f1b174d90b83eb267071d77655c44831072
Fred1706 <fred1942@outlook.fr>
2024-12-09 15:29:44 -0500

    Update SonHit.cs

--- Assets/SonHit.cs
+++ Assets/SonHit.cs
@@ -42,24 +42,20 @@ public class SonHit : MonoBehaviour
             AudioManager.GetComponent<RandomSons>().Hit();
             currentValue1 = infoPlayer1.life;
         }
-
-
-
-        if(infoPlayer2.life == currentValue2 - 1){
+        else if(infoPlayer2.life == currentValue2 - 1){
 
             AudioManager.GetComponent<RandomSons>().Hit();
             currentValue2 = infoPlayer2.life;
         }
-
-
-
-
-        if(infoPlayer1.life == 0 || infoPlayer2.life == 0){
-
-            currentValue2 = infoPlayer2.life;
+        else{
             currentValue1 = infoPlayer1.life;
-            
+            currentValue2 = infoPlayer2.life;
         }
+
+
+
+
+       
 
     }
 }
